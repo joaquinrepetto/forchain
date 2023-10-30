498b806e541b47e1d852b92f4c84e2864d7777ee
Joel Vargas <teslavargas18@gmail.com>
2023-10-30 13:59:37 -0300

    componente reutilizable

--- src/components/Global/globalStyles.js
+++ src/components/Global/globalStyles.js
@@ -36,3 +36,97 @@ export const PurpleButton = styled.button`
   color: white;
   font-size: 21px;
 `;
+
+// ---------------------------------------------------------  SCREEN COMPONENT
+
+export const ScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const TitleScreen = styled.h1`
+  color: ${theme.colors.nameText};
+  font-size: 20px;
+  font-family: "Codec Pro", sans-serif;
+  font-weight: 400;
+  margin-top: 17.5px;
+`;
+
+export const ScreenBodyLogged = styled.div``;
+
+export const ScreenTypesContainer = styled.div`
+  margin-top: 20px;
+`;
+
+export const ScreenTypes = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+  margin-bottom: 50px;
+`;
+
+export const ButtonScreenTypes = styled(({ isActive, ...rest }) => (
+  <button {...rest} />
+))`
+  border: none;
+  color: ${theme.colors.white};
+  font-size: 16px;
+  font-family: "Codec Pro", sans-serif;
+  font-weight: 400;
+  background-color: ${(props) =>
+    props.isActive ? `${theme.colors.border}` : `transparent`};
+  border-radius: 20px;
+  padding: 10px 20px;
+`;
+
+export const ScreenBodyUnlogged = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 80px;
+  width: 100%;
+`;
+
+export const SubtitleScreenContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-left: solid 3px ${theme.colors.purpleBorder};
+  justify-content: center;
+  align-items: center;
+  padding: 0px 10px;
+  width: 80%;
+  margin-bottom: 50px;
+`;
+
+export const SubtitleScreen = styled.h2`
+  color: ${theme.colors.nameText};
+  font-size: 22px;
+  font-family: "Codec Pro", sans-serif;
+  font-weight: 400;
+  margin: 0;
+`;
+
+export const ButtonLoginContainer = styled.div`
+  display: flex;
+  width: 100%;
+`;
+export const ButtonLogin = styled.button`
+  background-color: ${theme.colors.purpleBorder};
+  color: white;
+  padding: 15px 20px;
+  border: none;
+  border-radius: 17px;
+  cursor: pointer;
+  width: 100%;
+  margin-top: 20px;
+  font-family: "Poppins", sans-serif;
+  font-size: 20px;
+`;
---
+++ src/components/Screen/Screen.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  ScreenContainer,
+  TitleScreenContainer,
+  TitleScreen,
+  ScreenBodyLogged,
+  ScreenTypesContainer,
+  ScreenTypes,
+  ButtonScreenTypes,
+  ScreenBodyUnlogged,
+  SubtitleScreenContainer,
+  SubtitleScreen,
+  ButtonLoginContainer,
+  ButtonLogin,
+} from "../Global/globalStyles";
+
+const Screen = ({
+  user,
+  activeTab,
+  setActiveTab,
+  titleScreen,
+  tabLabels = [],
+  renderContent,
+  subtitle,
+  onClick,
+}) => {
+  return (
+    <ScreenContainer>
+      <TitleScreenContainer>
+        <TitleScreen>{titleScreen}</TitleScreen>
+      </TitleScreenContainer>
+      {user ? (
+        <ScreenBodyLogged>
+          <ScreenTypesContainer>
+            <ScreenTypes>
+              {tabLabels.map((label) => (
+                <ButtonScreenTypes
+                  isActive={activeTab === label}
+                  onClick={() => setActiveTab(label)}
+                >
+                  {label}
+                </ButtonScreenTypes>
+              ))}
+            </ScreenTypes>
+          </ScreenTypesContainer>
+          {renderContent(activeTab)}
+        </ScreenBodyLogged>
+      ) : (
+        <ScreenBodyUnlogged>
+          <SubtitleScreenContainer>
+            <SubtitleScreen>{subtitle}</SubtitleScreen>
+          </SubtitleScreenContainer>
+          <ButtonLoginContainer>
+            <ButtonLogin onClick={onClick}>Iniciar sesión</ButtonLogin>
+          </ButtonLoginContainer>
+        </ScreenBodyUnlogged>
+      )}
+    </ScreenContainer>
+  );
+};
+
+export default Screen;
