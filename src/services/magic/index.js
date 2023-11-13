2076452feebdd69b8f87a54259db354516eb8e50
Joel Vargas <teslavargas18@gmail.com>
2023-11-13 13:13:47 -0300

    using magic instance with extension

--- src/services/magic/index.js
+++ src/services/magic/index.js
@@ -1,10 +1,6 @@
 import { Magic } from "magic-sdk";
 import { AlgorandExtension } from "@magic-ext/algorand";
 
-export const magic = new Magic(process.env.REACT_APP_MAGIC_API_KEY, {
-  network: "goerli",
-});
-
 export const magicAlgorand = new Magic(process.env.REACT_APP_MAGIC_API_KEY, {
   extensions: {
     algorand: new AlgorandExtension({
